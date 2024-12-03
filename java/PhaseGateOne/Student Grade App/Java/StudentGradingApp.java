111676bf34fa70b04b26532d3a9c44c9a7dae08e
Uzochukwu <israeljohnson0464@gmail.com>
2024-12-03 13:53:08 +0100

    Men at work 4.0

--- java/PhaseGateOne/Student Grade App/Java/StudentGradingApp.java
+++ java/PhaseGateOne/Student Grade App/Java/StudentGradingApp.java
@@ -10,6 +10,11 @@ public class StudentGradingApp{
 	System.out.println("How many subjects do they offer ");
 	int numberOfSubjects = input.nextInt();
 
+	System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>>>>>");
+	System.out.println("Saved succesfully");
+	System.out.println();
+
+
 	double[][] studentsScoresheet = compileScores(numberOfStudents,numberOfSubjects);
 
 	
@@ -25,34 +30,44 @@ public class StudentGradingApp{
 		int student;
 		int course;
 		double score; 
-		double total = 0;
+		double total ;
 		double average = 0;
-		int scoreIndex = numberOfSubjects + 1;
-		int averageIndex = numberOfSubjects + 2;
+		int scoreIndex = numberOfSubjects ;
+		int averageIndex = numberOfSubjects + 1;
 		
 	
 		for(int studier = 0; studier < numberOfStudents; studier++){
 			student = studier + 1;
-			for(int subject = 0; subject < numberOfSubjects; subject++){
-				course = subject + 1;
-				System.out.println("Enter student"+student+" score in subject"+course);
-				score = input.nextDouble();
-				if(score >= 0 && score <= 100){
-					studentsScoresheet[studier][subject] = score;
-					total += score;
+			total = 0;
+			for(int subject = 0; subject < numberOfSubjectAndTotalWithAverage; subject++){
+				
+				if(subject < numberOfSubjects){
+					course = subject + 1;
+					System.out.println("Enter student"+student+" score in subject"+course);
+					score = input.nextDouble();
+					if(score >= 0 && score <= 100){
+						studentsScoresheet[studier][subject] = score;
+						total += score;
+					}else{
+						System.out.println("Wrong score input enter again:");
+						subject--;
+						continue;
+				     	}
+						
+					
+
+
 				}else{
-					System.out.println("Wrong score input enter again:");
-					subject--;
-					continue;
-				     }
-				if(subject == numberOfSubjects -1){
 					studentsScoresheet[studier][scoreIndex] = total;
-					studentsScoresheet[studier][averageIndex] = total/numberOfSubjects;
+					studentsScoresheet[studier][averageIndex] = average;
 				}
-			 
+
+							 
 		
 			}
-			
+			System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>>>>>");
+					System.out.println("Saved succesfully");
+					System.out.println();
 		}
 		return studentsScoresheet;
 	}
