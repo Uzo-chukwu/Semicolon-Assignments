4efe0cd552a599df56146e651133d9633a0aeae4
Uzochukwu <israeljohnson0464@gmail.com>
2025-03-04 03:50:40 +0100

    Inheritance class with mr chibuzor

---
+++ iLoveProgramming/JavaClass/Exercises/chibuzotasks/2025-03-03/matter/src/Animal.java
@@ -0,0 +1,8 @@
+public class Animals extends LivingThings {
+    private int numberOfHeads;
+    public Animals(String name, int numberOfHeads) {
+        super(name);
+        this.numberOfHeads = numberOfHeads;
+
+    }
+}
